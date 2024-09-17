26e3f6cfb48e96a3205dbe18159556f858d33059
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-09-16 23:56:26 -0700

    adding fonts

--- soda-app/src/app/layout.tsx
+++ soda-app/src/app/layout.tsx
@@ -1,5 +1,16 @@
+import localFont from "next/font/local";
+
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
+
+import "./app.css";
+
+const alpino = localFont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 900",
+  variable: "--font-alpino",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={alpino.variable}>
       <body>{children}</body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
