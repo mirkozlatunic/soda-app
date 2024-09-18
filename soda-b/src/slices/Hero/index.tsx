7b2c4b3b3477b7d00c57f1972225ed5b1906cad5
Mirko Zlatunic <m.zlatunic@gmail.com>
2024-09-17 19:25:18 -0700

    gsap scoll trigger setup

--- soda-b/src/slices/Hero/index.tsx
+++ soda-b/src/slices/Hero/index.tsx
@@ -5,12 +5,13 @@ import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 /**
  * Props for `Hero`.
@@ -35,7 +36,42 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       })
       .from(".hero-subheading", { opacity: 0, y: 30 }, "+=.8")
       .from(".hero-body", { opacity: 0, y: 10 })
-      .from(".hero-buttons", { opacity: 0, y: 10, duration: 0.6 });
+      .from(".hero-button", { opacity: 0, y: 10, duration: 0.6 });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+
+    scrollTl
+      .fromTo(
+        "body",
+        {
+          backgroundColor: "#FDE047",
+        },
+        {
+          backgroundColor: "#D9f99D",
+          overwrite: "auto",
+        },
+        1,
+      )
+      .from(".text-side-heading .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".text-side-body", {
+        y: 20,
+        opacity: 0,
+      });
   });
 
   return (
@@ -57,7 +93,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
             <div className="hero-subheading mt-12 text-5xl font-semibold text-sky-950 lg:text-6xl">
               <PrismicRichText field={slice.primary.subheading} />
             </div>
-            <div className="her-body text-2xl font-normal text-sky-950">
+            <div className="hero-body text-2xl font-normal text-sky-950">
               <PrismicRichText field={slice.primary.body} />
             </div>
             <Button
